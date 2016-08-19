e75e3da2ce92e2265dc3de5236cc3d9de183ec3d
Aron Bergman <aronbergman@ya.ru>
2016-08-19 11:31:49 +0500

    19.08

--- js/script.js
+++ js/script.js
@@ -20,3 +20,34 @@ $(document).ready(function () {
         return false;
     });
 });
+
+
+$(document).ready(function(){
+02
+    $("#top").on("click","a", function (event) {
+03
+        //отменяем стандартную обработку нажатия по ссылке
+04
+        event.preventDefault();
+05
+ 
+06
+        //забираем идентификатор бока с атрибута href
+07
+        var id  = $(this).attr('top'),
+08
+ 
+09
+        //узнаем высоту от начала страницы до блока на который ссылается якорь
+10
+            top = $(id).offset(10000).top;
+11
+         
+12
+        //анимируем переход на расстояние - top за 1500 мс
+13
+        $('body,html').animate({scrollTop: top}, 1500);
+14
+    });
+15
+});
--- mail.php
+++ mail.php
@@ -3,6 +3,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (isset($_POST['name'])) {$name = $_POST['name'];}
     if (isset($_POST['phone'])) {$phone = $_POST['phone'];}
     if (isset($_POST['formData'])) {$formData = $_POST['formData'];}
+    if (isset($_POST['text'])) {$text = $_POST['text'];}
 
     $to = "8rezvanov8@gmail.com";
     $sendfrom   = "8rezvanov8@gmail.com";
@@ -11,11 +12,21 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $headers .= "MIME-Version: 1.0\r\n";
     $headers .= "Content-Type: text/html;charset=utf-8 \r\n";
     $subject = "$formData";
-    $message = "$formData<br> <b>Имя пославшего:</b> $name <br><b>Телефон:</b> $phone";
+    $message = "$formData<br> <b>Имя пославшего:</b> $name <br><b>Телефон:</b> $phone <br><b>Сообщение:</b> $text";
     $send = mail ($to, $subject, $message, $headers);
+    $to = "a.mkr@icloud.com";//stat of form
+    $sendfrom   = "stat@bb-one.ru";
+    $headers  = "From: " . strip_tags($sendfrom) . "\r\n";
+    $headers .= "Reply-To: ". strip_tags($sendfrom) . "\r\n";
+    $headers .= "MIME-Version: 1.0\r\n";
+    $headers .= "Content-Type: text/html;charset=utf-8 \r\n";
+    $subject = "$formData";
+    $message = "$formData<br> <b>Имя пославшего:</b> $name <br><b>Телефон:</b> $phone <br><b>Сообщение:</b> $text";
+    $send = mail ($to, $subject, $message, $headers);
+
     if ($send == 'true')
     {
-    echo '<center><p class="success"><br>Спасибо за отправку вашего сообщения!</p></center>';
+    echo '<center><p class="success"><br>Спасибо за отправку, в ближайшее время мы позвоним!</p></center>';
     }
     else 
     {
